03bb08391330ec36bbcde279afd0e84034c85d02
Millerz8 <joe.h.miller@wustl.edu>
2025-02-28 11:50:16 -0600

    Replicated Results of ultimatum Game

--- GPT_AI.py
+++ GPT_AI.py
@@ -1,9 +1,37 @@
+import os
+import openai
 from openai import OpenAI
-client = OpenAI()
-completion = client.chat.completions.create(
-    model="gpt-4o",
-    store=True,
-    messages=[
-        {"role": "user", "content": "write a haiku about ai"}
-    ]
-)
+import pandas as pd
+from Ultimatum_Function import ultimatum_game
+import random
+import numpy as np
+
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+names = pd.read_csv("Surnames.csv")
+surnames = names[["Last Name"]]
+
+numprompts = 60
+
+player1s = random.sample(range(453), numprompts)
+player2s = random.sample(range(453), numprompts)
+
+
+title1s = np.random.choice([0, 1], size=numprompts, replace=True)
+title2s = np.random.choice([0, 1], size=numprompts, replace=True)
+titles = ["Mr.", "Ms."]
+
+names =[]
+yescount = 0
+for i in range(0, len(player1s)):
+    response = ultimatum_game(client, player1 = surnames.loc[player1s[i], "Last Name"], title1 = titles[title1s[i]], player2 = surnames.loc[player2s[i], "Last Name"], title2 = titles[title2s[i]])
+    if response == "Yes.":
+        yescount += 1
+
+acceptrate = yescount/numprompts
+print(acceptrate)
+
+
+
+
---
+++ Ultimatum_Function.py
@@ -0,0 +1,38 @@
+import os
+import openai
+from openai import OpenAI
+import pandas as pd
+
+#api_key = os.getenv("OPENAI_API_KEY")
+#client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+#client = OpenAI()
+
+def ultimatum_game(client, player1, title1, player2, title2):
+    player1 = title1 + " " + player1
+    player2 = title2 + " " + player2
+    if title1 == "Mr.":
+        pronoun = "himself"
+    else:
+        pronoun = "herself"
+
+    prompt = f"""In the following scenario, {player2} had to decide whether to accept or reject the proposal.
+
+    Scenario: {player1} is given $10. {player1} will propose how to split the money between {pronoun} and {player2}. 
+    Then {player2} will decide whether to accept or reject {player1}'s proposal. If {player2} accepts,
+    then {player1} and {player2} get the money as they agreed to split. If {player2} rejects, then both receive nothing.
+
+    {player1} takes $6 for himself and offers {player2} $4.
+
+    With a simple 'yes' or 'no' answer, does {player2} accept the proposal?"""
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": prompt}]
+    )
+
+    return completion.choices[0].message.content
+
+
+
+
